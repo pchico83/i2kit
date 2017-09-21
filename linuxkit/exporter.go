24c62354512b99ff4958a080a38d1e1c7a8e9b8a
Alberto <alberto@docker.com>
2017-09-21 15:10:57 -0700

    Linuxkit push raw image to S3 and create AMI

--- linuxkit/exporter.go
+++ linuxkit/exporter.go
@@ -2,18 +2,25 @@ package linuxkit
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"regexp"
 	"runtime"
+	"strings"
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/moby/tool/src/moby"
+	"github.com/pchico83/i2kit/utils"
 )
 
+var re utils.CustomRegex = utils.CustomRegex{regexp.MustCompile("Created AMI: (?P<ami>ami-[a-z0-9]+)")}
+
 // Build image from template with Moby tool
-func mobyBuild(mobyTemplate *moby.Moby, deploymentName string) error {
-	moby.MobyDir = filepath.Join(os.Getenv("HOME"), ".moby")
+func mobyBuild(mobyTemplate *moby.Moby, deploymentPath string) error {
+	log.Info(">>> Moby build started...")
 	var buf *bytes.Buffer
 	var w io.Writer
 	buf = new(bytes.Buffer)
@@ -23,24 +30,40 @@ func mobyBuild(mobyTemplate *moby.Moby, deploymentName string) error {
 		return err
 	}
 	image := buf.Bytes()
-	log.Infof("Create outputs:")
+	log.Infof("Create outputs in %s:", deploymentPath)
 	buildFormats := []string{"raw"} // AWS requires a RAW image
 	buildHyperkit := runtime.GOOS == "darwin"
-	return moby.Formats(filepath.Join(moby.MobyDir, deploymentName), image, buildFormats, 1024, buildHyperkit)
+	return moby.Formats(deploymentPath, image, buildFormats, 1024, buildHyperkit)
 }
 
 // Push image built by Moby to AWS S3 and then build the associated AMI
-func linuxkitPush() (string, error) {
-	return "ami", nil
+// (Needs linuxkit installed)
+func linuxkitPush(deploymentPath string) (string, error) {
+	log.Info(">>> Linuxkit run started... (this process will take a while)")
+	linuxkitPath, err := exec.Command("which", "linuxkit").Output()
+	if err != nil {
+		return "", fmt.Errorf("Could not find linuxkit in the system (Err: %s). Have you installed it? go get -u github.com/linuxkit/linuxkit/src/cmd/linuxkit", err)
+	}
+	linuxkitPathTrimmed := strings.Trim(string(linuxkitPath), "\n")
+	deploymentPathRaw := fmt.Sprintf("%s.raw", deploymentPath)
+	cmd := exec.Command(string(linuxkitPathTrimmed), "-v", "push", "aws", "-timeout", "1200", "-bucket", "linuxkit", deploymentPathRaw)
+	stdoutStderr, err := utils.StreamCommand(cmd)
+	if err != nil {
+		return "", fmt.Errorf("Error executing linuxkit command: %s", err)
+	}
+	output := re.FindStringSubmatchMap(stdoutStderr.String())
+	return output["ami"], nil
 }
 
 //Export generates a AWS ami from a linuxkit template
 func Export(mobyTemplate *moby.Moby, deploymentName string) (string, error) {
-	err := mobyBuild(mobyTemplate, deploymentName)
+	moby.MobyDir = filepath.Join(os.Getenv("HOME"), ".moby")
+	deploymentPath := filepath.Join(moby.MobyDir, deploymentName)
+	err := mobyBuild(mobyTemplate, deploymentPath)
 	if err != nil {
 		return "", err
 	}
-	ami, err := linuxkitPush()
+	ami, err := linuxkitPush(deploymentPath)
 	if err != nil {
 		return "", err
 	}
