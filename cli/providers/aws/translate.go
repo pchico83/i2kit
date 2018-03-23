bd84f158e0a3b0f7402960625e5b34398294fa0d
Pablo Chico de Guzmán <pchico83@gmail.com>
2018-03-23 20:02:24 +0100

    Switch to ecs base image (#59)

--- cli/providers/aws/translate.go
+++ cli/providers/aws/translate.go
@@ -16,7 +16,21 @@ import (
 )
 
 var amisPerRegion = map[string]string{
-	"us-west-2": "ami-7293320a",
+	"us-east-2":      "ami-1b90a67e",
+	"us-east-1":      "ami-cb17d8b6",
+	"us-west-2":      "ami-05b5277d",
+	"us-west-1":      "ami-9cbbaffc",
+	"eu-west-3":      "ami-914afcec",
+	"eu-west-2":      "ami-a48d6bc3",
+	"eu-west-1":      "ami-bfb5fec6",
+	"eu-central-1":   "ami-ac055447",
+	"ap-northeast-2": "ami-ba74d8d4",
+	"ap-northeast-1": "ami-5add893c",
+	"ap-southeast-2": "ami-4cc5072e",
+	"ap-southeast-1": "ami-acbcefd0",
+	"ca-central-1":   "ami-a535b2c1",
+	"ap-south-1":     "ami-2149114e",
+	"sa-east-1":      "ami-d3bce9bf",
 }
 
 // Translate an i2kit service to a AWS CloudFormation template
