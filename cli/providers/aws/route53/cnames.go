32989ca24a31a46fda6cbc8db235aaf2c3f0c541
Pablo Chico de Guzmán <pchico83@gmail.com>
2018-04-06 10:08:28 +0200

    Support for creating CNAMEs in a Root AWS account (#63)

---
+++ cli/providers/aws/route53/cnames.go
@@ -0,0 +1,53 @@
+package route53
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/route53"
+	"github.com/pchico83/i2kit/cli/providers/aws/cf"
+	"github.com/pchico83/i2kit/cli/schemas/environment"
+	"github.com/pchico83/i2kit/cli/schemas/service"
+)
+
+//Create creates a CNAME resolving to a service
+func Create(s *service.Service, e *environment.Environment) error {
+	return update(s, e, "UPSERT")
+}
+
+//Destroy destroys a CNAME resolving to a service
+func Destroy(s *service.Service, e *environment.Environment) error {
+	return update(s, e, "DELETE")
+}
+
+func update(s *service.Service, e *environment.Environment, action string) error {
+	target, err := cf.GetOutput(s.Name, "elbURL", e.Provider.GetConfig())
+	if err != nil {
+		return err
+	}
+	svc := route53.New(session.New(), e.DNSProvider.GetConfig())
+	recordName := fmt.Sprintf("%s.%s", s.Name, e.DNSProvider.HostedZone)
+	params := &route53.ChangeResourceRecordSetsInput{
+		ChangeBatch: &route53.ChangeBatch{
+			Changes: []*route53.Change{
+				{
+					Action: aws.String(action),
+					ResourceRecordSet: &route53.ResourceRecordSet{
+						Name: aws.String(recordName),
+						Type: aws.String("CNAME"),
+						ResourceRecords: []*route53.ResourceRecord{
+							{
+								Value: aws.String(target),
+							},
+						},
+						TTL: aws.Int64(900),
+					},
+				},
+			},
+		},
+		HostedZoneId: aws.String(e.DNSProvider.HostedZoneID),
+	}
+	_, err = svc.ChangeResourceRecordSets(params)
+	return err
+}
